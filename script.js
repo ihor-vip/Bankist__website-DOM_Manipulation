3456506cf796e07f8509754060387abba8aba54f
ihor-vip <vip20140902@gmail.com>
2024-02-09 11:39:43 -0600

    better , newer version for sticky nav implementing Intersection Observer API

--- script.js
+++ script.js
@@ -115,9 +115,27 @@ nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 
 // Sticky navigation
-const initialCoords = section1.getBoundingClientRect();
+// const initialCoords = section1.getBoundingClientRect();
 
-window.addEventListener("scroll", function (e) {
-  if (this.window.scrollY > initialCoords.top) nav.classList.add("sticky");
+// window.addEventListener("scroll", function (e) {
+//   if (this.window.scrollY > initialCoords.top) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// });
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+
+headerObserver.observe(header);
